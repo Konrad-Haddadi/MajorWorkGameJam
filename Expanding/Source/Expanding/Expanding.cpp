dc9df80b297f03d2b73b4c6b104d492aa5ba8d5d
Konrad-Haddadi <haddadikonrad@gmail.com>
2024-07-22 13:04:52 +1000

    Remade Project with Started Content

--- Expanding/Source/Expanding/Expanding.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Expanding.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Expanding, "Expanding" );
