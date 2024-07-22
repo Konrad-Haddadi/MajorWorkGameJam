4fa44063a9c4f8bada991257d92fcf8956e96765
Konrad-Haddadi <haddadikonrad@gmail.com>
2024-07-22 14:31:24 +1000

    Updated GitIgnore

---
+++ Unity/Unity_Expansion/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
