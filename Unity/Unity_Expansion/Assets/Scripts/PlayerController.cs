fdb6da1ba5db8a7742aad207313a219360a51484
Konrad-Haddadi <haddadikonrad@gmail.com>
2024-07-22 14:38:24 +1000

    Added Movement script

--- Unity/Unity_Expansion/Assets/Scripts/PlayerController.cs
+++ Unity/Unity_Expansion/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private InputActionReference movement;
+    [SerializeField] private float speed;
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveDir = movement.action.ReadValue<Vector2>();
+        rb.velocity = moveDir * speed;
     }
 }
